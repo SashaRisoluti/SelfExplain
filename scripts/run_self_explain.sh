549cb52bb270bec046150ae688b1c5c4230d3c2d
SashaRisoluti <164863971+SashaRisoluti@users.noreply.github.com>
2024-07-03 16:07:30 +0200

    Update run_self_explain.sh

--- scripts/run_self_explain.sh
+++ scripts/run_self_explain.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 export TOKENIZERS_PARALLELISM=false
 python model/run.py --dataset_basedir data/XLNet-SUBJ \
-                         --lr 2e-5  --max_epochs 5 \
-                         --gpus 1 \
+                         --lr 2e-5  --min_lr 1e-5  --max_epochs 5 \
+                         --h_dim 256  --h_heads 8  --kqv_dim 64 \
+                         --weight_decay 0.0001  --warmup_drop 0.1 \
+                         --num_gpus 1  --batch_size 16 \
+                         --clip_grad 1.0  
                          --concept_store data/XLNet-SUBJ/concept_store.pt \
                          --accelerator ddp \
                          --gamma 0.1 \
