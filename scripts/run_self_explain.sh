6ae4d9ccc73fafdb1a6e2be15a87896a6627e852
SashaRisoluti <164863971+SashaRisoluti@users.noreply.github.com>
2024-07-03 16:11:04 +0200

    Update run_self_explain.sh

--- scripts/run_self_explain.sh
+++ scripts/run_self_explain.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 export TOKENIZERS_PARALLELISM=false
-python model/run.py --dataset_basedir data/XLNet-SUBJ \
-                         --lr 2e-5  --min_lr 1e-5  --max_epochs 5 \
-                         --h_dim 256  --h_heads 8  --kqv_dim 64 \
-                         --weight_decay 0.0001  --warmup_drop 0.1 \
-                         --num_gpus 1  --batch_size 16 \
-                         --clip_grad 1.0  
-                         --concept_store data/XLNet-SUBJ/concept_store.pt \
-                         --accelerator ddp \
-                         --gamma 0.1 \
-                         --lamda 0.1 \
-                         --topk 5
+python model/run.py \
+    --num_gpus 1 \
+    --batch_size 32 \
+    --clip_grad 1.0 \
+    --dataset_basedir data/XLNet-SUBJ \
+    --concept_store data/XLNet-SUBJ/concept_store.pt \
+    --model_name my_model \
+    --gamma 0.1 \
+    --lamda 0.5 \
+    --topk 5 \
+    --min_lr 1e-5 \
+    --h_dim 256 \
+    --n_heads 8 \
+    --kqv_dim 64 \
+    --num_classes 10 \
+    --lr 0.001 \
+    --weight_decay 0.0001 \
+    --warmup_prop 0.1
+                         
+                         
 
 # for RoBERTa
 # python model/run.py --dataset_basedir data/RoBERTa-SUBJ \
