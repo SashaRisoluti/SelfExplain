2c679a713dac447c595bf36db71f976fa0f88a5f
SashaRisoluti <164863971+SashaRisoluti@users.noreply.github.com>
2024-07-09 11:55:06 +0200

    Update run_preprocessing.sh

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -1,18 +1,18 @@
-#export DATA_FOLDER='data/XLNet-SUBJ'
-#export TOKENIZER_NAME='xlnet-base-cased'
-#export MAX_LENGTH=5
+export DATA_FOLDER='data/XLNet-SUBJ'
+export TOKENIZER_NAME='xlnet-base-cased'
+export MAX_LENGTH=5
 
 # Creates jsonl files for train and dev
 
 python preprocessing/store_parse_trees.py \
-      --data_dir data/XLNet-SUBJ  \
-      --tokenizer_name xlnet-base-cased
+      --data_dir $DATA_FODLER  \
+      --tokenizer_name $TOKENIZER_NAME
 
 # Create concept store for SST-2 dataset
 # Since SST-2 already provides parsed output, easier to do it this way, for other datasets, need to adapt
 
 python preprocessing/build_concept_store.py \
-       -i data/XLNet-SUBJ/train_with_parse.json \
-       -o data/XLNet-SUBJ \
-       -m xlnet-base-cased \
-       -l 5
+       -i $DATA_FODLER/train_with_parse.json \
+       -o $DATA_FODLER \
+       -m $TOKENIZER_NAME \
+       -l $MAX_LENGTH
